32c38dc9fac7769e5cafd17a7a846706726b32c3
kajuvileladantas <kajuvileladantas@gmail.com>
2026-03-06 13:50:30 -0300

    feat: Update Firebase configuration
    Updated Firebase configuration with new project details and API keys. This change is necessary for connecting to the correct Firebase project.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6CIZSnkQpLGXhZJZfJRbWNINmIty8c58",
-  authDomain: "amoreplanejamento0204.firebaseapp.com",
-  projectId: "amoreplanejamento0204",
-  storageBucket: "amoreplanejamento0204.firebasestorage.app",
-  messagingSenderId: "901699360119",
-  appId: "1:901699360119:web:476ebe723591689fd8fd72",
-  measurementId: "G-ZSZ3T9QHR2"
+  apiKey: "AIzaSyA7gocPkzEK6cx06873KikSOLhM_rlLwiI",
+  authDomain: "amoreplanejamento2026.firebaseapp.com",
+  projectId: "amoreplanejamento2026",
+  storageBucket: "amoreplanejamento2026.firebasestorage.app",
+  messagingSenderId: "866600127528",
+  appId: "1:866600127528:web:8c146174cc7041710c391d",
+  measurementId: "G-XLFJHTRFL2"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
